f2190c77e65a4fb7f4598801ab0137dc7cce0d0d
aaabdoulay <aaabdoulay@turing.local.isima.fr>
2022-11-05 08:48:23 +0100

    Code de base

---
+++ TheatricalPlayers/src/main/java/Principale.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Principale {
+
+    public static void main(String[] args) {
+        StatementPrinter imprimante = new StatementPrinter();
+        Play piece_tragique = new Play("Les miserables etudiants", "tragedy");
+        Play fary = new Play("Noir is the new black", "comedy");
+        Play blanche = new Play("Blanche", "comedy");
+
+        Performance perf1 = new Performance("mis01", 2000);
+        Performance perf2 = new Performance("mis01", 1200);
+        Performance perf3 = new Performance("com01", 5000);
+        Performance perf4 = new Performance("com02", 3100);
+        
+        List<Performance> liste_perf = new ArrayList<Performance>();
+        liste_perf.add(perf1);
+        liste_perf.add(perf2);
+        liste_perf.add(perf3);
+        liste_perf.add(perf4);
+
+        Map<String, Play> plays = new HashMap<String, Play>();
+        plays.put("com02", blanche);
+        plays.put("com01", fary);
+        plays.put("mis01", piece_tragique);
+        plays.put("mis01", piece_tragique);
+
+
+        Invoice clermont = new Invoice("Mohamed Yacine", liste_perf);
+
+        System.out.println(imprimante.print(clermont, plays));
+
+
+    }
+}
